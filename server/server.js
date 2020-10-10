eacbdc9d91b3617dde3df34602c05f5b279af52a
Denis Gil <denisgilsil.21@gmail.com>
2020-10-10 15:23:50 -0500

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 require('./config/config');
 const express = require('express');
+
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -9,34 +12,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario/:id', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err) => {
+    if (err) {
+        throw err;
     }
-    res.json({
-        persona: body
-    })
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+    console.log('Base de datos online');
 });
 
 app.listen(process.env.PORT, () => {
