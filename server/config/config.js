69bacc19e0e765fdbb230ed746937e85b3cd627d
Denis Gil <denisgilsil.21@gmail.com>
2020-10-10 16:09:16 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://denis:Nqj3t0CdPPisjT5b@cluster0.lzmod.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
